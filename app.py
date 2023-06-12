fe73c1d4f0d8638f51245033da665fec63a99a81
Novman_47 <mohammed.novman@gmail.com>
2023-06-12 13:14:55 +0530

    trigger

--- app.py
+++ app.py
@@ -3,4 +3,4 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Please subscribe, like, and comment on this video, TY!!!'
+    return 'Triggering deployment from git webhook and in sysn with Argo-CD!!!'
